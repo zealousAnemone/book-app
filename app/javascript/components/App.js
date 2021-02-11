91918f21da42e746500b8ce5ea74f582d3df0179
Veronica Stork <zealous.anemone@gmail.com>
2021-02-11 15:53:43 -0500

    First commit after setup

---
+++ app/javascript/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Main from './Main';
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <Main />
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
